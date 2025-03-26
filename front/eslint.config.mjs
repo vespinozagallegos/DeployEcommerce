c0c8d88b41ffdcac32ecffd7fd80239d3c7b2742
Victor <espinozagallegosvictor@gmail.com>
2025-03-26 00:20:19 -0300

    Se limpia el warning existente por falta de un plugin

--- front/eslint.config.mjs
+++ front/eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin"; // Usar import
+import nextPlugin from "@next/eslint-plugin-next";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,6 +15,7 @@ export default {
   ...compat.extends("next/core-web-vitals", "next/typescript")[0], // Asegurar que sea un objeto
   plugins: {
     "@typescript-eslint": typescriptEslintPlugin, // Se agrega el plugin como objeto
+    "@next/next": nextPlugin
   },
   rules: {
     "@typescript-eslint/no-explicit-any": "error",
